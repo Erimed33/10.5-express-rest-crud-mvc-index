0d5eeceab8dca9a6e60864bd19ba05d7fdfe469e
Erimed33 <erikamedina@pursuit.org>
2024-06-23 20:50:42 -0400

    variables-setup-appjs

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 const express = require('express')
 const app = express()
 
+const locationModels = require('./models/locationModels')
+const machineModels = require('./models/machineModels')
+const personModels = require('./models/personModels')
+const planModels = require('./models/planModel')
+const specialEventsModel= require('./models/specialEventsModel')
 
-
+// Routes
+// home
 app.get('/', (req, res) => {
     res.send(`Hello, world!`)
+})
+
+// variables resource
+const locationsController = 
+
+// not found 404
+
+app.get('*', (req, res) => {
+    res.status(404).json({ error: "Page not found"})
 })
 
 
